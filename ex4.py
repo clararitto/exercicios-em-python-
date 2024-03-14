d7a6cc201b4a9550afb8eb0b1ec6fdf159a03c94
Clara :) <128714704+clararitto@users.noreply.github.com>
2024-03-14 10:05:07 -0300

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,3 +1,7 @@
+"""
+Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+"""
+
 nota1=float(input("Digite sua primeira nota : "))
 nota2=float(input("Digite sua segunda nota : "))
 nota3=float(input("Digite sua terceira nota : "))
