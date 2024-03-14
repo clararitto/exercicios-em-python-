572b7e6757cc5c1545469cdb8fe8c064788c2d19
Clara :) <128714704+clararitto@users.noreply.github.com>
2024-03-14 11:43:09 -0300

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,5 @@
+base = float(input("Digite a base do quadrado : "))
+altura = float(input("Digite a altura do triângulo : "))
+area= base*altura
+dobro=area*2
+print(f"a area do seu quadrado é {area} e o dobro dela é {dobro}")
