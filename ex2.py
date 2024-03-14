e8c9e1cd8e57a0a1612a7ecab8e1787e1bce61f4
Clara :) <128714704+clararitto@users.noreply.github.com>
2024-03-14 10:03:10 -0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,2 +1,7 @@
+"""
+Faça um Programa que peça um número e então mostre a mensagem
+O número informado foi [número].
+"""
+
 num= input("Digite um numero : ")
 print (f" O NUMERO INFORMADO FOI {num}")
