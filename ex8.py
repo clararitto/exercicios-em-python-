ea8148980e8f29c6c82b51c7ea58988878e9b6b6
Clara :) <128714704+clararitto@users.noreply.github.com>
2024-03-15 08:39:24 -0300

    Add files via upload

---
+++ ex8.py
@@ -0,0 +1,4 @@
+hrt =float(input("Quanto você recebe por hora trabalhada? "))
+hrm =float(input("Quantas horas por mês você trabalha? "))
+saltotal= (hrt*hrm)
+print(f"você ganha {saltotal} no mês")
