91e54cfcce5f4c4100ac3714cc8dfac67af3698d
Clara :) <128714704+clararitto@users.noreply.github.com>
2024-03-15 10:22:38 -0300

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,11 @@
+inteiro1=int(input("Digite um numero inteiro: "))
+inteiro2=int(input("Digite outro numero inteiro: "))
+real=float(input("Digite um numero real: "))
+
+resultado1= (2*inteiro1)*(inteiro2/2)
+resultado2= (3*inteiro1)+(real)
+resultado3= real**3
+
+print(f"O produto do dobro do primeiro com metade do segundo é : {resultado1}" )
+print(f"A soma do triplo do primeiro com o terceiro é : {resultado2}" )
+print(f"O terceiro elevado ao cubo é : {resultado3}")
